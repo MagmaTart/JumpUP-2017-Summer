cca6d68f1a7a3d81ae39961a032ef78a76020dd9
Soomin Lee <magmatart@gmail.com>
2017-08-02 06:26:12 -0700

    source update

--- mole-project/src/mole.c
+++ mole-project/src/mole.c
@@ -10,7 +10,10 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 #include <stdlib.h>
+
+#define CPU_CLOCK 16000000
 
 /* To control Mole LEDs easily */
 #define DDR_MOLE     DDRB
@@ -23,10 +26,8 @@
 #define LEVEL_PORT_E        PORTE
 #define BUZZER              PORT6
 
-/* About TIMER */
-#define CPU_CLOCK       16000000
-#define TICKS_PER_SEC   1000        
-#define PRESCALER 64
+#define BAUD_RATE       19200
+#define BAUD_RATE_L     (CPU_CLOCK / (16l * BAUD_RATE)) -1
 
 /* To remember each mole's position */
 #define MOLE0 PORT7
@@ -34,22 +35,17 @@
 #define MOLE2 PORT5
 #define MOLE3 PORT4
 
-volatile unsigned int tick = 0;
 volatile unsigned char led = 0x00;                       // Mole LEDs' status
 volatile unsigned int catched = 0;                       // Catched mole number
 volatile unsigned int missed = 0;                        // Missed mole number
 volatile unsigned int level = 1;                         // Game Level : High is more difficult
 volatile unsigned int next_mole_living_times = 1500;     // Living time of mole that will be setting new 
 volatile unsigned int next_mole_set_delay = 1500;        // Delay of set new moles : Use to Level Up
-//volatile unsigned int level_port_D = 0x00;               // Use PD6, PD7 to show difficulty (Green, Yellow)
-//volatile unsigned int level_port_E = 0x00;               // Use PE7 to show difficulty (Red)
 
 volatile unsigned int mole_status[4] = {0, 0, 0, 0};                        // Is that mole live? (Live : 1, Died : 0)
 volatile unsigned int mole_living_times[4] = {1500, 1500, 1500, 1500};      // Current Living time of mole in that position
 
 volatile unsigned int new_mole_delay = 1500;             // Delay of set new moles
-
-void delay(unsigned int);
 
 /* Interrupt Initializing */
 void init_interrupt()
@@ -59,24 +55,31 @@ void init_interrupt()
 
     EICRA |= (1<<ISC01) | (0<<ISC00) | (1<<ISC11) | (0<<ISC10) | (1<<ISC21) | (0<<ISC20) | (1<<ISC31) | (0<<ISC30);
     EIMSK |= (1<<INT0) | (1<<INT1) | (1<<INT2) | (1<<INT3);
-
-    /* Timer/Counter0 Initializing */
-    /* Output Compare Match */
-    TCCR0A = (1<<WGM01) | (0<<WGM00);
-    TCCR0B = (0<<WGM02) | (0<<CS02) | (1<<CS01) | (1<<CS00);        //Prescaler : 64 (011)
-    TCNT0 = 0;
-    OCR0A = (CPU_CLOCK / TICKS_PER_SEC / PRESCALER) - 1;
-    TIMSK0 = (0<<OCIE0B) | (1<<OCIE0A) | (0<<TOIE0);
 }
 
-void buzzer(){
+void buzzer()
+{
     int i=0;
-    while(i++<100)
+    while(i<50)
     {
         PORTD |= (1<<BUZZER);
-        delay(1);
-        PORTD |= (0<<BUZZER);
-        delay(1);
+        _delay_us(700);
+        PORTD &= ~((0<<BUZZER) | 0xFF);
+        _delay_us(700);
+        i++;
+    }
+}
+
+void buzzer_high()
+{
+    int i=0;
+    while(i<50)
+    {
+        PORTD |= (1<<BUZZER);
+        _delay_us(500);
+        PORTD &= ~((0<<BUZZER) | 0xff);
+        _delay_us(500);
+        i++;
     }
 }
 
@@ -86,11 +89,11 @@ void kill_mole(int num)
 
     if(mole_status[num])
     {
-        led &= ~((1 << moles[num]) & 0xff);
-        PORT_MOLE = led;
+        PORT_MOLE &= ~((1 << moles[num]) & 0xff);
         catched++;
         buzzer();
-        mole_status[0] = 0;
+        mole_status[num] = 0;
+        mole_living_times[num] = 0;
     }
 }
 
@@ -118,19 +121,6 @@ ISR(INT3_vect)
     kill_mole(3);
 }
 
-/* Output Compare : Matched */
-ISR(TIMER0_COMPA_vect)
-{
-    tick++;
-}
-
-/* Custom delay function using Timer */
-void delay(unsigned int msec)
-{
-    tick = 0;
-    while(msec > tick);
-}
-
 /* Set new mole and show */
 void new_mole(int new)
 {
@@ -139,22 +129,20 @@ void new_mole(int new)
 
     switch(new){
     case 0:
-        led |= (1<<MOLE0);
+        PORT_MOLE |= (1<<MOLE0);
         break;
     case 1:
-        led |= (1<<MOLE1);
+        PORT_MOLE |= (1<<MOLE1);
         break;
     case 2:
-        led |= (1<<MOLE2);
+        PORT_MOLE |= (1<<MOLE2);
         break;
     case 3:
-        led |= (1<<MOLE3);
+        PORT_MOLE |= (1<<MOLE3);
         break;
     default:
         break;
     }
-
-    PORT_MOLE = led;
 }
 
 /* Kill old mole that over its living time */
@@ -162,22 +150,21 @@ void shutdown_mole(int mole_number)
 {
     switch(mole_number){
     case 0:
-        led &= ~((1<<MOLE0) & 0xff);
+        PORT_MOLE &= ~((1<<MOLE0) & 0xff);
         break;
     case 1:
-        led &= ~((1<<MOLE1) & 0xff);
+        PORT_MOLE &= ~((1<<MOLE1) & 0xff);
         break;
     case 2:
-        led &= ~((1<<MOLE2) & 0xff);
+        PORT_MOLE &= ~((1<<MOLE2) & 0xff);
         break;
     case 3:
-        led &= ~((1<<MOLE3) & 0xff);
+        PORT_MOLE &= ~((1<<MOLE3) & 0xff);
         break;
     }
 
     missed++;
     mole_status[mole_number] = 0;
-    PORT_MOLE = led;
 }
 
 /* Increase difficulty */
@@ -218,7 +205,14 @@ void level_up(void)
 
 void gameover(void)
 {
-    //TODO : make this part
+    int i=0;
+    for(i=0;i<3;i++)
+    {
+        PORT_MOLE |= (1<<PORT4) | (1<<PORT5) | (1<<PORT6) | (1<<PORT7);
+        buzzer_high();
+        PORT_MOLE &= ~(((1<<PORT4) | (1<<PORT5) | (1<<PORT6) | (1<<PORT7)) & 0xff);
+        _delay_ms(150);
+    }
 }
 
 int main(void)
@@ -246,7 +240,7 @@ int main(void)
 
 
     /* Start Motion is not implemented, replaced to delay function */
-    delay(3000);
+    _delay_ms(3000);
 
     /* Set start mole and show */
     new_mole(rand() % 4);
@@ -254,7 +248,7 @@ int main(void)
     while(1)
     {
         /* Global delay 1ms */
-        delay(1);
+        _delay_ms(1);
         
         for(i=0;i<4;i++)
         {
@@ -262,6 +256,10 @@ int main(void)
             if( mole_status[i] ){
                 mole_living_times[i]--;
                 if( !mole_living_times[i] ) shutdown_mole(i);       //Living time over : shutdown mole
+            }
+            else
+            {
+                mole_living_times[i] = 0;
             }
         }
 
@@ -284,9 +282,10 @@ int main(void)
             level_up();
         }
 
-        if( missed >= 15)
+        if( missed >= 15 )
         {
             gameover();
+            break;
         }
     }
 }
